ebfec6f6430a00b4729654d82f4125193b40b996
mehdi <dehghani_m@rocketmail.com>
2022-09-01 15:45:31 +0430

    make dagger hilt

---
+++ app/src/main/java/com/example/trackapp/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.example.trackapp.di
+
+import android.content.Context
+import android.provider.DocumentsContract
+import androidx.room.Room
+import com.example.trackapp.db.RunningDataBase
+import com.example.trackapp.other.Constants.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent ::class)
+object AppModule {
+   ......
+
+}
+
+
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app : Context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDataBase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db : RunningDataBase) = db.getRunDao()
+
+}
