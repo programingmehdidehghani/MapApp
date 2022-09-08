7e7bee1b5ca6c8b66291cbb9308c2d93b92062c8
mehdi <dehghani_m@rocketmail.com>
2022-09-08 13:28:58 +0430

    try 2 make service

--- app/src/main/java/com/example/trackapp/services/TrackingService.kt
+++ app/src/main/java/com/example/trackapp/services/TrackingService.kt
@@ -1,9 +1,23 @@
 package com.example.trackapp.services
 
+import android.content.Intent
 import androidx.lifecycle.LifecycleService
+import com.example.trackapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import timber.log.Timber
 
 class TrackingService : LifecycleService(){
 
-    
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let {
+            when(intent.action){
+                ACTION_START_OR_RESUME_SERVICE -> {
+                   Timber.d("started or resume service")
+                }
+            }
+        }
+
+
+        return super.onStartCommand(intent, flags, startId)
+    }
 
 }
