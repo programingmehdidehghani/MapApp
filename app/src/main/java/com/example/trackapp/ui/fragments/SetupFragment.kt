a3d8c068864579d5b35b23e7bcd47b61d3a5f7e3
mehdi <dehghani_m@rocketmail.com>
2022-09-08 10:44:57 +0430

    make BottomNavigation

--- app/src/main/java/com/example/trackapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/SetupFragment.kt
@@ -1,7 +1,21 @@
 package com.example.trackapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.trackapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 }
