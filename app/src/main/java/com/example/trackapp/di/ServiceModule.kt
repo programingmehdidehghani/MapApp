bc39ca400cd587c1a2bddcce95ed4be2b6d89ed3
mehdi <dehghani_m@rocketmail.com>
2022-09-15 12:22:50 +0430

    make service module

--- app/src/main/java/com/example/trackapp/di/ServiceModule.kt
+++ app/src/main/java/com/example/trackapp/di/ServiceModule.kt
@@ -1,11 +1,42 @@
 package com.example.trackapp.di
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.example.trackapp.other.Constants
+import com.example.trackapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
 
 
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext app : Context
+    ) = FusedLocationProviderClient(app)
+
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    @ServiceScoped
+    @Provides
+    fun provideMainActivityPendingIntent(
+        @ApplicationContext app: Context
+    ) = PendingIntent.getActivity(
+        app,
+        0,
+        Intent(app, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 }
