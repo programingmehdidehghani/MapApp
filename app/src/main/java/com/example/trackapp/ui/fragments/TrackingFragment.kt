6c11733163601f2cf1e99df932f6da028d58f3a6
mehdi <dehghani_m@rocketmail.com>
2022-09-15 16:14:04 +0430

    finish dialog

--- app/src/main/java/com/example/trackapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/TrackingFragment.kt
@@ -85,6 +85,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private fun toggleRun () {
         if (isTracking){
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
@@ -140,6 +141,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnFinishRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
