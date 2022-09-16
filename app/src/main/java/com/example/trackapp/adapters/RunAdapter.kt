77dc80cfee5c438354e348d2ef65b8499380d159
mehdi <dehghani_m@rocketmail.com>
2022-09-16 21:33:49 +0430

    make diffutils

--- app/src/main/java/com/example/trackapp/adapters/RunAdapter.kt
+++ app/src/main/java/com/example/trackapp/adapters/RunAdapter.kt
@@ -2,11 +2,28 @@ package com.example.trackapp.adapters
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.trackapp.db.Run
 
 class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>(){
 
     inner class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>(){
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this,diffCallback)
+
+    fun submitList(list: List<Run>) = differ.submitList(list)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
         TODO("Not yet implemented")
