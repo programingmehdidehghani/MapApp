95019eb35ca081fe102a7e9226bc3f2acfe6bda1
mehdi <dehghani_m@rocketmail.com>
2022-09-16 22:05:06 +0430

    finish insert db

--- app/src/main/java/com/example/trackapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/RunFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trackapp.R
@@ -29,6 +30,11 @@ class RunFragment : Fragment(R.layout.fragment_run) , EasyPermissions.Permission
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
         setupRecyclerView()
+
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
+
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
