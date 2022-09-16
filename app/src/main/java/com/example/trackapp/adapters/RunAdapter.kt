f54f29f47e25a6104794521162234d69e8bbece0
mehdi <dehghani_m@rocketmail.com>
2022-09-16 21:25:01 +0430

    implement method recyclerview

--- app/src/main/java/com/example/trackapp/adapters/RunAdapter.kt
+++ app/src/main/java/com/example/trackapp/adapters/RunAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.trackapp.adapters
 
-class RunAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>(){
+
+    inner class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
