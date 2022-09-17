02e984681179d4571c7b8d02bb096c661ed7ca53
mehdi <dehghani_m@rocketmail.com>
2022-09-17 14:14:12 +0430

    finish this episod

--- app/src/main/java/com/example/trackapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/SettingsFragment.kt
@@ -24,7 +24,7 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        loadFieldsFromSharedPref()
        btnApplyChanges.setOnClickListener {
            val success = applyChangesTpSharedPref()
            if (success){
