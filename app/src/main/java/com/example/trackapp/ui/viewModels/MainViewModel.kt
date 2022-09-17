65b7c8ea07857ad6e7f1a4179dd4f9ca0ec63f46
mehdi <dehghani_m@rocketmail.com>
2022-09-17 11:37:00 +0430

    try2 make funtion sors in vewmodel

--- app/src/main/java/com/example/trackapp/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/trackapp/ui/viewModels/MainViewModel.kt
@@ -28,7 +28,31 @@ class MainViewModel @Inject constructor(
     val sortType = SortType.DATA
 
     init {
-        
+        runs.addSource(runsSortedByDate){ result ->
+            if (sortType == SortType.DATA){
+                result?.let { runs.value = it }
+            }
+        }
+        runs.addSource(runsSortedByAvgSpeed){ result ->
+            if (sortType == SortType.AVG_SPEED){
+                result?.let { runs.value = it }
+            }
+        }
+        runs.addSource(runsSortedByCaloriesBurned){ result ->
+            if (sortType == SortType.CALORIES_BURNED){
+                result?.let { runs.value = it }
+            }
+        }
+        runs.addSource(runsSortedByDistance){ result ->
+            if (sortType == SortType.DISTANCE){
+                result?.let { runs.value = it }
+            }
+        }
+        runs.addSource(runsSortedByTimeInMillis){ result ->
+            if (sortType == SortType.RUNNING_TIME){
+                result?.let { runs.value = it }
+            }
+        }
     }
 
 
