9b8856df1a069bee18b91995ca4f38b1476fef4f
mehdi <dehghani_m@rocketmail.com>
2022-09-17 13:40:06 +0430

    make shread in setting fragment

--- app/src/main/java/com/example/trackapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,33 @@
 package com.example.trackapp.ui.fragments
 
+import android.content.SharedPreferences
 import androidx.fragment.app.Fragment
 import com.example.trackapp.R
+import com.example.trackapp.other.Constants.KEY_NAME
+import com.example.trackapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private fun applyChangesTpSharedPref() : Boolean{
+        val nameText = etName.text.toString()
+        val weightText = etWeight.text.toString()
+        if (nameText.isEmpty() || weightText.isEmpty()){
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME,nameText)
+            .putFloat(KEY_WEIGHT,weightText.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
+    }
 }
