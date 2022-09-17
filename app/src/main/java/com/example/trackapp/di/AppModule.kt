fde437268cd13756cae0e1166b699f34f3b64f83
mehdi <dehghani_m@rocketmail.com>
2022-09-17 12:53:03 +0430

    make funtion di

--- app/src/main/java/com/example/trackapp/di/AppModule.kt
+++ app/src/main/java/com/example/trackapp/di/AppModule.kt
@@ -2,9 +2,13 @@ package com.example.trackapp.di
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.provider.DocumentsContract
 import androidx.room.Room
 import com.example.trackapp.db.RunningDataBase
+import com.example.trackapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.trackapp.other.Constants.KEY_NAME
+import com.example.trackapp.other.Constants.KEY_WEIGHT
 import com.example.trackapp.other.Constants.RUNNING_DATABASE_NAME
 import com.example.trackapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
@@ -39,5 +43,18 @@ object AppModule {
     fun providesSharedPreferences(@ApplicationContext app: Context){
         app.getSharedPreferences(SHARED_PREFERENCES_NAME,MODE_PRIVATE)
     }
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref : SharedPreferences) = sharedPref.getString(KEY_NAME,"") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref : SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref : SharedPreferences) = sharedPref.getBoolean(
+        KEY_FIRST_TIME_TOGGLE, true) 
 
 }
