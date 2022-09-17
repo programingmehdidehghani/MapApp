b9c236d13910fe111133060cd3c992ba79c29df1
mehdi <dehghani_m@rocketmail.com>
2022-09-17 12:40:02 +0430

    make shared for name weight

--- app/src/main/java/com/example/trackapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragments/SetupFragment.kt
@@ -1,5 +1,6 @@
 package com.example.trackapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.trackapp.R
+import com.example.trackapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.trackapp.other.Constants.KEY_NAME
+import com.example.trackapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    @Inject
+    lateinit var sharedPref : SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,5 +28,21 @@ class SetupFragment : Fragment(R.layout.fragment_setup) {
         tvContinue.setOnClickListener {
             findNavController().navigate(R.id.action_setupFragment_to_runFragment)
         }
+    }
+
+    private fun writePersonalDataToSharedPref() : Boolean{
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPref.edit()
+            .putString(KEY_NAME,name)
+            .putFloat(KEY_WEIGHT,weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE,false)
+            .apply()
+        val toolbarText ="Let's go, $name!"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
     }
 }
