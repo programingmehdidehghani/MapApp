361dafda1f5cba352806695965a52b207e859872
mehdi <dehghani_m@rocketmail.com>
2022-09-22 10:55:26 +0330

    make dialog fragment

---
+++ app/src/main/java/com/example/trackapp/ui/fragments/CancelTrackingDialog.kt
@@ -0,0 +1,12 @@
+package com.example.trackapp.ui.fragments
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class CancelTrackingDialog : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
